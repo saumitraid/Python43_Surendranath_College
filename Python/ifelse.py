54b0ffff10b440a419a8d10a4d9d652c4a4c4176
saumitraid <saumitraid.das@gmail.com>
2025-05-27 10:50:35 +0530

    22-05-2025

---
+++ Python/ifelse.py
@@ -0,0 +1,6 @@
+if False:
+    print("If Condition is True")
+elif True:
+    print("Else if is True")
+else:
+    print("This is else block")
---
+++ Python/swapping.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter 1st Number:-"))
+n2=int(input("Enter 2nd Number:-"))
+print("Before swapping n1=%d and n2=%d"%(n1,n2))
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print("After swapping n1=%d and n2=%d"%(n1,n2))
